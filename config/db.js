a964e10d957f9e4781916922d4eb5a8ce98f9107
orcatt <haotianzheng04@gmail.com>
2024-11-25 15:30:03 +0800

    更新数据连接地址

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2');
 
 // 创建数据库连接池
 const pool = mysql.createPool({
-  host: 'localhost',    // 数据库地址
-  user: 'root',         // 数据库用户名
-  password: '19991129', // 数据库密码
-  database: 'crayon_bs',  // 数据库名称
+  host: '111.119.235.36',    // 线上数据库地址
+  user: 'orcatt',             // 数据库用户名
+  password: 'Zht231822',      // 数据库密码
+  database: 'crayon_bs',      // 数据库名称
   waitForConnections: true,
-  connectionLimit: 10,  // 连接池最大连接数
+  connectionLimit: 10,        // 连接池最大连接数
   queueLimit: 0
 });
 
